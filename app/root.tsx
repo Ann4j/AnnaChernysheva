25059564d8ddb28e24571f91b054837aeb28d808
Анна Чернышева <annacernyseva@MacBook-Air-Anna-2.local>
2024-09-08 19:28:56 +0300

    favicon

--- app/root.tsx
+++ app/root.tsx
@@ -16,6 +16,7 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <link rel="icon" href="favicon.svg?v=2" />
       </head>
       <body className="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
         <Outlet />
